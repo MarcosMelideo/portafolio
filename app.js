fdb55b76b971865130d036ca579ec1b6570753c2
Marcos Melideo <marcosmelideo@gmail.com>
2025-08-27 17:44:41 -0300

    actualizacion del portafolio

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-particlesJS("particles-js", {"particles":{"number":{"value":160,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":1,"random":true,"anim":{"enable":true,"speed":1,"opacity_min":0,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":4,"size_min":0.3,"sync":false}},"line_linked":{"enable":false,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":1,"direction":"none","random":true,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":600}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"bubble"},"onclick":{"enable":false,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":250,"size":0,"duration":2,"opacity":0,"speed":3},"repulse":{"distance":400,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
+for (let i = 0; i < 700; i++) {
+    const element = document.createElement("div");
+    element.classList.add("star");
+    const x = Math.random() * window.innerWidth;
+    const y = Math.random() * window.innerHeight;
+    const size = Math.random() * 3;
+
+    element.style.top = `${y}px`;
+    element.style.left = `${x}px`;
+    element.style.width = `${size}px`;
+    element.style.height = `${size}px`;
+
+    document.querySelector('.animation-section').appendChild(element);
+}
+
+for(let e=0; e<300; e++){
+    const x=Math.floor(Math.random()*300);
+    document.querySelectorAll(".star")[x].classList.add('twinkle');
+}
 
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
